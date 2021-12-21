a36c9be67c76ef70f0cb66e4647df41adf0bc9c0
mahmoudHidowi <93486297+mahmoudHidowi@users.noreply.github.com>
2021-12-21 21:46:15 +0300

    Add files via upload

---
+++ admin/manage-admin.php
@@ -0,0 +1,14 @@
+<?php include('partials/menu.php'); ?>
+
+    <!-- main content section starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>manage admin</h1>
+    
+
+
+        </div>
+    </div>
+    <!-- main content section ends -->
+
+    <?php include('partials/footer.php'); ?>
---
+++ admin/partials/menu.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>food order website - home page</title>
+        
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+    <!-- menu section starts -->
+    <div class= "menu text-center">
+        <div class= "wrapper">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">food</a></li>
+                <li><a href="manage-order.php">order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- menu section ends -->
