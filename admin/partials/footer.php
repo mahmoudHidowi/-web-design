d5090d3569e9130859ad31e7755c481eeb768459
mahmud handevi <93486297+mahmoudHidowi@users.noreply.github.com>
2021-12-22 02:10:27 +0300

    Update footer.php

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,7 +1,7 @@
 <!-- footer section starts -->
 <div class="footer">
     <div class="wrapper">
-        <p class="text-center">2020 All rights reserved, Food House. Developed by - <a href="#">mahmud ve dima</a></p>
+        <p class="text-center">2020 All rights reserved, Food House. Developed by - <a href="#">mahmud handevi</a></p>
         </div>
     </div>
     <!-- footer section ends -->
